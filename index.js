f1929d7cb584cac49de38edbc23666dc68adc8a7
iamakeem <bolarinwa_akeem@yahoo.com>
2023-04-24 23:10:26 +0100

    firbase Database

---
+++ index.js
@@ -0,0 +1,21 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js';
+import { getDatabase, ref, push } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
+
+const appSettings = {
+  databaseURL:
+    'https://playground-5e617-default-rtdb.europe-west1.firebasedatabase.app/',
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const dataInDB = ref(database, 'data')
+
+const inputField = document.getElementById('input-field');
+const addButton = document.getElementById('add-button');
+
+addButton.addEventListener('click', function () {
+  let inputValue = inputField.value;
+  
+  push(dataInDB, inputValue)
+  console.log(inputValue);
+});
